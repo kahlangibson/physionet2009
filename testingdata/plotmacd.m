ba1da37a13045689d12df5325516bf86af18b9e6
mpftesta <mpftesta@yahoo.com>
2018-03-18 16:30:09 -0700

    Added plotmacd.m file to training and test data

---
+++ testingdata/plotmacd.m
@@ -0,0 +1,13 @@
+clf;
+ema12 = tsmovavg(DAT(:,1).', 'e', 12);
+ema26 = tsmovavg(DAT(:,1).', 'e', 26);
+macd = ema12-ema26;
+sig = tsmovavg(macd(26:660), 'e', 9);
+yyaxis left;
+plot(macd(26:660),'r');
+hold on;
+plot(sig, 'b');
+yyaxis right;
+plot(DAT(26:660,1).', 'black');
+plot(DAT(26:660,3).', 'black');
+hold off;
